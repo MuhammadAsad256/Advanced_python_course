dbf9d9ff2290ce3c5b0f562bcada9307299e6162
Muhammad_Asad <156535903+MuhammadAsad256@users.noreply.github.com>
2024-08-21 22:30:33 +0500

    Update views.py

--- Assignment_06/numeric_api/views.py
+++ Assignment_06/numeric_api/views.py
@@ -8,7 +8,9 @@ from functools import reduce
 def calculate_total(request):
     data = json.loads(request.body)
     numbers = data.get('numbers')
+    
     # total_sum = sum(numbers)
+    
     total_sum = reduce(lambda x, y: x + y, numbers)
     return JsonResponse({'numbers':total_sum})
 
@@ -17,7 +19,9 @@ def calculate_total(request):
 def calculate_average(request):
     data = json.loads(request.body)
     numbers = data.get('numbers')
+    
     # average = sum(numbers) / len(numbers)
+    
     average = reduce(lambda x, y: x + y, numbers) / len(numbers)
     return JsonResponse({'numbers': average})
 
@@ -26,10 +30,10 @@ def calculate_product(request):
     data = json.loads(request.body)
     numbers = data.get('numbers')
 
-    # def multiply(x, y):
-    #     return x * y
-    # total_product = reduce(multiply, numbers)
-    # return JsonResponse({'numbers': total_product})
+    # total_product = 1
+
+    # for number in numbers:
+    #     total_product = total_product * number
 
     total_product = reduce(lambda x, y: x * y, numbers)
     return JsonResponse({'numbers':total_product})
